af9e164e8788d285175cf924a4ee35b1d0b35001
omer levkovich <omerlevkovich@gmail.com>
2018-12-26 18:18:11 +0200

    Add files via upload

--- distance_and_buzzer.ino
+++ distance_and_buzzer.ino
@@ -1,13 +1,9 @@
 /*
-HC-SR04 Ping distance sensor]
 VCC to arduino 5v GND to arduino GND
-Echo to Arduino pin 13 Trig to Arduino pin 12
-Red POS to Arduino pin 11
-Green POS to Arduino pin 10
-560 ohm resistor to both LED NEG and GRD power rail
-More info at: http://goo.gl/kJ8Gl
-Original code improvements to the Ping sketch sourced from Trollmaker.com
-Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+Echo to Arduino pin 12 Trig to Arduino pin 11
+Led to pin 13
+Buzzer to plus to pin 9
+Buzzer minus to ground
 */
 
 #define trigPin 11
