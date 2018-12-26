810ecd361ad131531653f497e0b6c0a746420104
omer levkovich <omerlevkovich@gmail.com>
2018-12-26 18:15:38 +0200

    Add files via upload

---
+++ distance_and_buzzer.ino
@@ -0,0 +1,48 @@
+/*
+HC-SR04 Ping distance sensor]
+VCC to arduino 5v GND to arduino GND
+Echo to Arduino pin 13 Trig to Arduino pin 12
+Red POS to Arduino pin 11
+Green POS to Arduino pin 10
+560 ohm resistor to both LED NEG and GRD power rail
+More info at: http://goo.gl/kJ8Gl
+Original code improvements to the Ping sketch sourced from Trollmaker.com
+Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+*/
+
+#define trigPin 11
+#define echoPin 12
+#define led 13
+#define speakerPin 9
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode (speakerPin, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 40) {  // This is where the LED On/Off happens
+    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+    tone(speakerPin, 10);
+    delay (500);
+    noTone(speakerPin);
+    delay(10);
+}
+  else {
+    digitalWrite(led,LOW);
+  }
+
+  delay(500);
+//   digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+}
